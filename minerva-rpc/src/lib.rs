5505df004e471cab3d238de8e8c0dd49ab816463
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-05-06 19:44:17 -0300

    Add helper elements for creating a `PRODUCT` client

--- minerva-rpc/src/lib.rs
+++ minerva-rpc/src/lib.rs
@@ -31,6 +31,29 @@ pub mod users {
 
 pub mod products {
     tonic::include_proto!("product");
+
+    use super::metadata::ClientInterceptor;
+    use tonic::codegen::InterceptedService;
+    use tonic::transport::Channel;
+
+    pub type ProductsInterceptedService =
+        products_client::ProductsClient<InterceptedService<Channel, ClientInterceptor>>;
+
+    pub async fn make_client(
+        endpoint: String,
+        tenant: String,
+        requestor: String,
+    ) -> ProductsInterceptedService {
+        let channel = Channel::from_shared(endpoint.clone())
+            .unwrap()
+            .connect()
+            .await
+            .unwrap();
+        products_client::ProductsClient::with_interceptor(
+            channel,
+            ClientInterceptor::new(&tenant, &requestor),
+        )
+    }
 }
 
 pub mod metadata {
