647ac1c1b3ac1b648684c45eb909fe24c2927cb5
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-05-06 23:51:16 -0300

    Prepare `PRODUCT` for logging and multi-tenancy

--- minerva-product/src/service.rs
+++ minerva-product/src/service.rs
@@ -1,4 +1,7 @@
+use minerva_data as lib_data;
+use minerva_rpc as lib_rpc;
 use minerva_rpc::messages;
+use minerva_rpc::metadata;
 use minerva_rpc::products::products_server::Products;
 use tonic::{Request, Response, Status};
 
@@ -8,6 +11,19 @@ pub struct ProductsService;
 #[tonic::async_trait]
 impl Products for ProductsService {
     async fn index(&self, req: Request<()>) -> Result<Response<messages::ProductList>, Status> {
+        let tenant = metadata::get_value(req.metadata(), "tenant").ok_or(
+            Status::failed_precondition("Missing tenant on request metadata"),
+        )?;
+        let requestor = metadata::get_value(req.metadata(), "requestor").ok_or(
+            Status::failed_precondition("Missing requestor on request metadata"),
+        )?;
+        lib_data::log::print(
+            lib_rpc::get_address(&req),
+            requestor.clone(),
+            tenant.clone(),
+            "PRODUCT::INDEX",
+        );
+
         unimplemented!();
     }
 
@@ -15,6 +31,19 @@ impl Products for ProductsService {
         &self,
         req: Request<messages::EntityIndex>,
     ) -> Result<Response<messages::Product>, Status> {
+        let tenant = metadata::get_value(req.metadata(), "tenant").ok_or(
+            Status::failed_precondition("Missing tenant on request metadata"),
+        )?;
+        let requestor = metadata::get_value(req.metadata(), "requestor").ok_or(
+            Status::failed_precondition("Missing requestor on request metadata"),
+        )?;
+        lib_data::log::print(
+            lib_rpc::get_address(&req),
+            requestor.clone(),
+            tenant.clone(),
+            "PRODUCT::SHOW",
+        );
+
         unimplemented!();
     }
 
@@ -22,6 +51,19 @@ impl Products for ProductsService {
         &self,
         req: Request<messages::Product>,
     ) -> Result<Response<messages::Product>, Status> {
+        let tenant = metadata::get_value(req.metadata(), "tenant").ok_or(
+            Status::failed_precondition("Missing tenant on request metadata"),
+        )?;
+        let requestor = metadata::get_value(req.metadata(), "requestor").ok_or(
+            Status::failed_precondition("Missing requestor on request metadata"),
+        )?;
+        lib_data::log::print(
+            lib_rpc::get_address(&req),
+            requestor.clone(),
+            tenant.clone(),
+            "PRODUCT::STORE",
+        );
+
         unimplemented!();
     }
 
@@ -29,10 +71,36 @@ impl Products for ProductsService {
         &self,
         req: Request<messages::Product>,
     ) -> Result<Response<messages::Product>, Status> {
+        let tenant = metadata::get_value(req.metadata(), "tenant").ok_or(
+            Status::failed_precondition("Missing tenant on request metadata"),
+        )?;
+        let requestor = metadata::get_value(req.metadata(), "requestor").ok_or(
+            Status::failed_precondition("Missing requestor on request metadata"),
+        )?;
+        lib_data::log::print(
+            lib_rpc::get_address(&req),
+            requestor.clone(),
+            tenant.clone(),
+            "PRODUCT::UPDATE",
+        );
+
         unimplemented!();
     }
 
     async fn delete(&self, req: Request<messages::EntityIndex>) -> Result<Response<()>, Status> {
+        let tenant = metadata::get_value(req.metadata(), "tenant").ok_or(
+            Status::failed_precondition("Missing tenant on request metadata"),
+        )?;
+        let requestor = metadata::get_value(req.metadata(), "requestor").ok_or(
+            Status::failed_precondition("Missing requestor on request metadata"),
+        )?;
+        lib_data::log::print(
+            lib_rpc::get_address(&req),
+            requestor.clone(),
+            tenant.clone(),
+            "PRODUCT::DELETE",
+        );
+
         unimplemented!();
     }
 }
--- minerva-user/src/tests.rs
+++ minerva-user/src/tests.rs
@@ -49,7 +49,7 @@ async fn make_test_server(
 
     tokio::time::sleep(Duration::from_millis(100)).await;
 
-    let client = minerva_rpc::users::make_client(endpoint, "minerva".into(), "tests".into()).await;
+    let client = minerva_rpc::users::make_client(endpoint, "minerva".into(), "tester".into()).await;
 
     (handle, client, tx)
 }
