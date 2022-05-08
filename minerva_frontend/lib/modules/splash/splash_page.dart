c6369d0fb22c64fc52a3caede85c23d8629e6362
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-05-08 18:30:03 -0300

    Add text input example and logo

--- minerva_frontend/lib/modules/splash/splash_page.dart
+++ minerva_frontend/lib/modules/splash/splash_page.dart
@@ -20,47 +20,71 @@ class SplashPage extends StatelessWidget {
             color: const Color(0xFFDFE0E7),
           ),
           padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 50.0),
-          child: Column(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
                 // Minerva logo
                 Container(
                   child: Neumorphic(
                     style: NeumorphicStyle(
                       shape: NeumorphicShape.concave,
-                      boxShape: NeumorphicBoxShape.circle(),
+                      boxShape: const NeumorphicBoxShape.circle(),
                       depth: -10,
                       intensity: 0.9,
                       color: const Color(0xFFDEE1ED),
                     ),
-                    padding: const EdgeInsets.all(50.0),
+                    padding: const EdgeInsets.all(15.0),
+                    child: Image.asset('assets/images/logo.png'),
                   ),
-                  padding: const EdgeInsets.only(top: 30.0, bottom: 50.0),
+                  padding: const EdgeInsets.only(bottom: 50.0),
                 ),
                 // Writable input
-                Container(
+               Material(
+                 color: const Color(0x00FFFFFF),
+                  child: Container(
+                    child: Neumorphic(
+                      style: NeumorphicStyle(
+                        shape: NeumorphicShape.flat,
+                        boxShape: NeumorphicBoxShape.roundRect(
+                            BorderRadius.circular(50)),
+                        depth: -2,
+                        intensity: 1.5,
+                        color: const Color(0xFFDEE1ED),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 5.0),
+                        child: TextField(
+                          obscureText: false,
+                          decoration: InputDecoration(
+                            isDense: true,
+                            labelText: "Login",
+                            labelStyle:
+                                const TextStyle(color: Color(0xFF9A9A9A)),
+                            filled: false,
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ),
+                    ),
+                    padding: const EdgeInsets.only(bottom: 25.0),
+                  ),
+                ),
+                // Writable input
+              Container(
                   child: Neumorphic(
                     style: NeumorphicStyle(
                       shape: NeumorphicShape.flat,
-                      boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(50)),
+                      boxShape: NeumorphicBoxShape.roundRect(
+                          BorderRadius.circular(50)),
                       depth: -2,
                       intensity: 1.5,
                       color: const Color(0xFFDEE1ED),
                     ),
-                    padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 200.0),
-                  ),
-                  padding: const EdgeInsets.only(bottom: 25.0),
-                ),
-                // Writable input
-                Container(
-                  child: Neumorphic(
-                    style: NeumorphicStyle(
-                      shape: NeumorphicShape.flat,
-                      boxShape: NeumorphicBoxShape.roundRect(BorderRadius.circular(50)),
-                      depth: -2,
-                      intensity: 1.5,
-                      color: const Color(0xFFDEE1ED),
-                    ),
-                    padding: const EdgeInsets.symmetric(vertical: 25.0, horizontal: 200.0),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 25.0, horizontal: 200.0),
                   ),
                   padding: const EdgeInsets.only(bottom: 25.0),
                 ),
@@ -81,6 +105,7 @@ class SplashPage extends StatelessWidget {
                   ),
                 ),
               ],
+            ),
           ),
         ),
       ),
