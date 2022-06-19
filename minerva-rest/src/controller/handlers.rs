5c29bb49e40ffe7487e79039d53ca252b6be8c21
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-06-19 20:35:14 -0300

    Add documentation for error handlers

--- minerva-rest/src/controller/handlers.rs
+++ minerva-rest/src/controller/handlers.rs
@@ -1,9 +1,13 @@
+//! This submodule contains default status code catchers for errors within the
+//! API.
+
 use super::response::Response;
 use rocket::http::ContentType;
 use rocket::http::Status;
 use rocket::Request;
 use serde_json::json;
 
+/// Exports an array of catcher functions to be used when mounting the server.
 pub fn catchers() -> Vec<rocket::Catcher> {
     catchers![
         default_catcher,
@@ -16,7 +20,9 @@ pub fn catchers() -> Vec<rocket::Catcher> {
     ]
 }
 
+/// Default error structure implementing a Rocket responder for any status code.
 struct DefaultError {
+    /// Status code for the error.
     status: Status,
 }
 
@@ -33,15 +39,19 @@ impl<'r> rocket::response::Responder<'r, 'static> for DefaultError {
     }
 }
 
+/// Generates a simple JSON string from a given message.
 fn gen_message(message: &str) -> String {
     json!({ "message": message }).to_string()
 }
 
+/// Default error catcher for any kind of error that was not treated
+/// specifically.
 #[catch(default)]
 fn default_catcher<'r>(status: Status, _req: &Request) -> DefaultError {
     DefaultError { status }
 }
 
+/// Error catcher for 400 - Bad Request.
 #[catch(400)]
 fn bad_request(status: Status, _req: &Request) -> Response {
     Response::BadRequest(gen_message(&format!(
@@ -50,6 +60,8 @@ fn bad_request(status: Status, _req: &Request) -> Response {
     )))
 }
 
+/// Error catcher for 401 - Unauthorized.
+/// Returned on problems with the authentication fairing.
 #[catch(401)]
 fn unauthorized(status: Status, _req: &Request) -> Response {
     Response::Unauthorized(gen_message(&format!(
@@ -58,6 +70,7 @@ fn unauthorized(status: Status, _req: &Request) -> Response {
     )))
 }
 
+/// Error catcher for 404 - Not Found.
 #[catch(404)]
 fn not_found(status: Status, _req: &Request) -> Response {
     Response::NotFound(gen_message(&format!(
@@ -66,6 +79,8 @@ fn not_found(status: Status, _req: &Request) -> Response {
     )))
 }
 
+/// Error catcher for 412 - Unprocessable Entity.
+/// Returned when a JSON body to a request cannot be deserialized.
 #[catch(422)]
 fn unprocessable_entity(status: Status, _req: &Request) -> Response {
     Response::UnprocessableEntity(gen_message(&format!(
@@ -74,6 +89,7 @@ fn unprocessable_entity(status: Status, _req: &Request) -> Response {
     )))
 }
 
+/// Error catcher for 500 - Internal Server Error.
 #[catch(500)]
 fn internal(status: Status, _req: &Request) -> Response {
     Response::InternalServerError(gen_message(&format!(
@@ -82,6 +98,8 @@ fn internal(status: Status, _req: &Request) -> Response {
     )))
 }
 
+/// Error catcher for 503 - Service Unavailable.
+/// Returned whenever a microservice cannot be reached to complete the request.
 #[catch(503)]
 fn service_unavailable(status: Status, _req: &Request) -> Response {
     Response::ServiceUnavailable(gen_message(&format!(
