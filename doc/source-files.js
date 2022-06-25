7e995f7fdfbb63992bc19b6bde50a88476931611
luksamuk <lucasvieira@protonmail.com>
2022-06-25 22:56:37 +0000

    Deploying to gh-pages from @ luksamuk/minerva-system@565a8ad5d2f1f77aa39acc34e5861ac4a121ded8 🚀

--- doc/source-files.js
+++ doc/source-files.js
@@ -1,5 +1,5 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["minerva_cache"] = {"name":"","files":["lib.rs"]};
+sourcesIndex["minerva_cache"] = {"name":"","files":["auth.rs","lib.rs"]};
 sourcesIndex["minerva_data"] = {"name":"","dirs":[{"name":"db","files":["create.rs","mod.rs"]}],"files":["encryption.rs","file.rs","lib.rs","log.rs","mongo.rs","schema.rs","session.rs","syslog.rs","tenancy.rs","user.rs"]};
 sourcesIndex["minerva_product"] = {"name":"","files":["main.rs","service.rs"]};
 sourcesIndex["minerva_report"] = {"name":"","files":["main.rs"]};
