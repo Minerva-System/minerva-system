f40a6afed18949334fb00e8df5b577ffc90c6086
Lucas S. Vieira <lucasvieira@protonmail.com>
2022-06-27 19:05:41 -0300

    Add automatic tagging in image generation

--- generate_images.sh
+++ generate_images.sh
@@ -11,31 +11,28 @@ for TARGET in "${TARGETS[@]}"
 do
     IMGNAME="luksamuk/${TARGET}"
 
-    # Generate image and tag it with "latest"
-    echo "### Building $IMGNAME..."
-    docker image build -f build/Dockerfile \
-	   --target $TARGET \
-	   -t $IMGNAME:latest .;
-
     # Scrape version information and use as tag
     DIRNAME=${TARGET/_/-}
     IMGVERSION=`awk '/^version/{print $3}' ./$DIRNAME/Cargo.toml | tr -d '"'`;
     TAGGEDIMGNAME=$IMGNAME:$IMGVERSION
-
-    # Tag image according to Cargo.toml
-    echo "### Tagging $TAGGEDIMGNAME..."
-    docker image tag $IMGNAME $TAGGEDIMGNAME
+    
+    # Generate image and tag it with "latest"
+    echo "### Building $IMGNAME..."
+    docker image build \
+	    -f build/Dockerfile \
+	    --target $TARGET \
+	    -t $TAGGEDIMGNAME \
+	    -t $IMGNAME:latest \
+	    .
 done
 
 # FrontEnd target
 IMGVERSION=`awk '/^version/{print $2}' ./minerva_frontend/pubspec.yaml`
 echo "### Building luksamuk/minerva_frontend..."
 docker image build -f build/Dockerfile.frontend \
+       -t luksamuk/minerva_frontend:$IMGVERSION \
        -t luksamuk/minerva_frontend:latest \
        .
-
-echo "### Tagging luksamuk/minerva_frontend:$IMGVERSION"
-docker image tag luksamuk/minerva_frontend luksamuk/minerva_frontend:$IMGVERSION
 
 # PgAdmin
 docker image build -f build/Dockerfile.pgadmin \
